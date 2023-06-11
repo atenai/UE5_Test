aacb72e673f627cfba9b37e1ddc50833312a0ed8
LAPTOP-LA51FEJM\alfam <alfamod47@yahoo.co.jp>
2023-06-12 00:24:28 +0900

    関数ライブラリの金型作成

---
+++ Source/CPP_BP/Public/CPP_MyLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CPP_MyLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPP_BP_API UCPP_MyLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/CPP_BP/Public/MathLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MathLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPP_BP_API UMathLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
