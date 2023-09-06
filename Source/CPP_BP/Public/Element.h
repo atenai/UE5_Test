c711c1ad881e3e31c0e3e4356de776f784418e5e
LAPTOP-LA51FEJM\alfam <alfamod47@yahoo.co.jp>
2023-09-06 20:05:11 +0900

    C++のenumを追加

---
+++ Source/CPP_BP/Public/Element.h
@@ -0,0 +1,12 @@
+// ファイル名：Element.h 
+#pragma once 
+
+#include "CoreMinimal.h" 
+#include "Element.generated.h" 
+
+UENUM(BlueprintType) 
+enum class EElement : uint8 
+{ 
+	Subtract  UMETA(DisplayName = "Subtraction"),
+};
+
