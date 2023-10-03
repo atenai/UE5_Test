5764a5528730078e90baa348faa3bec4258ddbcc
LAPTOP-LA51FEJM\alfam <alfamod47@yahoo.co.jp>
2023-10-03 19:12:50 +0900

    ポーズ画面を表示

---
+++ Source/CPP_BP/Public/CPP_MyObject.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "CPP_MyObject.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPP_BP_API UCPP_MyObject : public UObject
+{
+	GENERATED_BODY()
+	
+};
