8061d72abb43c8d9c3e869a6c2cab552c1bac9e6
LAPTOP-LA51FEJM\alfam <alfamod47@yahoo.co.jp>
2023-10-07 21:16:48 +0900

    C++でオブジェクトを作成

--- Source/CPP_BP/Private/CPP_BallPlayer.cpp
+++ Source/CPP_BP/Private/CPP_BallPlayer.cpp
@@ -17,23 +17,50 @@ ACPP_BallPlayer::ACPP_BallPlayer()
 	Sphere = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComponent"));
 	RootComponent = Sphere;
 
-	// StaticMeshをLaodしてStaticMeshComponentのStaticMeshに設定する
-	//ポインタ型の読み込む型 M変数名 = LoadObject<読み込む型>(nullptr, TEXT("読み込む物のパス"));
+	// StaticMeshをLoadしてStaticMeshComponentのStaticMeshに設定する
+	//ポインタ型の読み込む型 変数名 = LoadObject<読み込む型>(nullptr, TEXT("読み込む物のパス"));
 	UStaticMesh* Mesh = LoadObject<UStaticMesh>(nullptr, TEXT("/Engine/BasicShapes/Sphere"));
 
 	// StaticMeshをStaticMeshComponentに設定する
 	//Sphere->と書けばSphereのコンポーネントにアクセスできる！
+	//変数名->SetStaticMesh(セットするメッシュの変数);
 	Sphere->SetStaticMesh(Mesh);
 
 	// MaterialをStaticMeshに設定する
+	//ポインタ型の読み込む型 変数名 = LoadObject<読み込む型>(nullptr, TEXT("読み込む物のパス"));
 	UMaterial* Material = LoadObject<UMaterial>(nullptr, TEXT("/Engine/BasicShapes/BasicShapeMaterial"));
 
 	// MaterialをStaticMeshComponentに設定する
+	//変数名->SetMaterial(複数のマテリアルを設定できる為、何番目か？を指定する, セットするマテリアルの変数);
 	Sphere->SetMaterial(0, Material);
 
 	// Simulate Physicsを有効にする
+	//変数名->SetSimulatePhysics(trueなら有効、falseなら無効);
 	Sphere->SetSimulatePhysics(true);
 
+	// SpringArmを追加する
+	//変数名 = CreateDefaultSubobject<追加するコンポーネント>(TEXT("表示したい名前"));
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+	//変数名->SetupAttachment(親オブジェクトにしたいオブジェクトが格納された変数);//指定した親オブジェクトの子オブジェクトにする
+	SpringArm->SetupAttachment(RootComponent);
+
+	// 角度を変更する FRotator(Pitch(Y), Yaw(Z), Roll(X))
+	SpringArm->SetRelativeRotation(FRotator(-30.0f, 0.0f, 0.0f));
+
+	// Spring Armの長さを調整する
+	SpringArm->TargetArmLength = 450.0f;
+
+	// SpringArmからの角度を継承しない
+	SpringArm->bInheritPitch = false;
+	SpringArm->bInheritYaw = false;
+	SpringArm->bInheritRoll = false;
+
+	// CameraのLagを有効にする
+	SpringArm->bEnableCameraLag = true;
+
+	// Cameraを追加する
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	Camera->SetupAttachment(SpringArm);
 }
 
 // Called when the game starts or when spawned
