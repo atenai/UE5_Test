c99283da69fb7f7a9e03549e7859cdfdd1c955c0
LAPTOP-LA51FEJM\alfam <alfamod47@yahoo.co.jp>
2023-10-23 20:53:02 +0900

    C++でUIを作成

---
+++ Source/CPP_BP/Public/MoveParam.h
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MoveParam.generated.h"
+
+USTRUCT(BlueprintType) 
+struct FMoveParam
+{
+	GENERATED_BODY()
+};
