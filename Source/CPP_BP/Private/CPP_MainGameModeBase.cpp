96e719bc2ba36b90b36b431d1dbf1acc2b19b6d7
LAPTOP-LA51FEJM\alfam <alfamod47@yahoo.co.jp>
2023-11-08 20:24:00 +0900

    ゲームモードを追加

--- Source/CPP_BP/Private/CPP_MainGameModeBase.cpp
+++ Source/CPP_BP/Private/CPP_MainGameModeBase.cpp
@@ -7,7 +7,7 @@
 ACPP_MainGameModeBase::ACPP_MainGameModeBase(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
 	//ブループリントのキャラクターを取得
-	static ConstructorHelpers::FClassFinder<APawn> PawnClass(TEXT("/Game/Blueprints/BP_PlayerPawn"));
+	static ConstructorHelpers::FClassFinder<APawn> PawnClass(TEXT("/Game/Blueprints/BP_PlayerPawn.BP_PlayerPawn_C"));
 
 	//取得に成功したら中身を実行する
 	if (PawnClass.Succeeded())
