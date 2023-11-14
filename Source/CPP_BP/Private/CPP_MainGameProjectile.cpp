d68c9d869684fd909da3aa67ca1829820ab5eb66
LAPTOP-LA51FEJM\alfam <alfamod47@yahoo.co.jp>
2023-11-14 14:45:39 +0900

    弾の移動関数を作成

---
+++ Source/CPP_BP/Private/CPP_MainGameProjectile.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CPP_MainGameProjectile.h"
+
+// Sets default values
+ACPP_MainGameProjectile::ACPP_MainGameProjectile() :Super()
+{
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	RootComponent = StaticMeshComponent;
+
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ACPP_MainGameProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACPP_MainGameProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ACPP_MainGameProjectile::MoveProjectile(const FVector& Direction, float DeltaTime)
+{
+	//このオブジェクトの座標をセットする（方向 × 時間）+ 現在地
+	SetActorLocation((Direction * DeltaTime) + GetActorLocation());
+}
