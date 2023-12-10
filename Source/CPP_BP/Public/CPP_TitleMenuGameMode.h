9426101fb9f165b73e1a4b079868506fa350c04d
LAPTOP-LA51FEJM\alfam <alfamod47@yahoo.co.jp>
2023-12-10 22:06:40 +0900

    列挙型クラスを作成

---
+++ Source/CPP_BP/Public/CPP_TitleMenuGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CPP_TitleMenuGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPP_BP_API ACPP_TitleMenuGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/CPP_BP/Public/CPP_TitleMenuPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "CPP_TitleMenuPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPP_BP_API ACPP_TitleMenuPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
