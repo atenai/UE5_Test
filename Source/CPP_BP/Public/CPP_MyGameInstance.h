27b68bde69007cc5372b80d146fc78e30723f610
LAPTOP-LA51FEJM\alfam <alfamod47@yahoo.co.jp>
2023-12-16 23:58:10 +0900

    ゲームインスタンスを作成

---
+++ Source/CPP_BP/Public/CPP_MyGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "CPP_MyGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPP_BP_API UCPP_MyGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
