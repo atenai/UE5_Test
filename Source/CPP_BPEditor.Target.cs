c263f74773318c55c09a198a08ca98affd0c7e11
LAPTOP-LA51FEJM\alfam <alfamod47@yahoo.co.jp>
2025-01-03 02:07:33 +0900

    5.4へバージョンを上げる

--- Source/CPP_BPEditor.Target.cs
+++ Source/CPP_BPEditor.Target.cs
@@ -8,8 +8,8 @@ public class CPP_BPEditorTarget : TargetRules
 	public CPP_BPEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("CPP_BP");
 	}
 }
